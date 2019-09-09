b980e9c9db396d9c52313a50072ba1be8ca0a1a5
Adrian Gomez <aegomez@tekmexico.com>
2019-09-09 16:09:32 -0700

    Add webpack-demo - Output Management

--- ch12/webpack-demo/src/index.js
+++ ch12/webpack-demo/src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml'
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
@@ -14,6 +16,11 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  btn.textContent = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   console.log(Data);
 
